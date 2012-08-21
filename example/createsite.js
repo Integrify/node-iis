3b83ab38de7acc1f1a4d67e22809a17c12cef313
Patrick Dillon <dillon76@gmail.com>
2012-08-21 15:38:17 -0400

    Initial commit.

---
+++ example/createsite.js
@@ -0,0 +1,18 @@
+var iis = require('iis');
+
+//
+// create a new site on port 80
+//
+iis.createSite({
+	                  name:'NewSite',
+					  protocol: 'http',
+					  host: '*',
+					  port: 80,
+					  path: 'c:\\mynewsite'
+					},
+					function(err,stdout) {
+                       if (!err) {
+                           console.log('Site created');
+                       }
+
+                    });
